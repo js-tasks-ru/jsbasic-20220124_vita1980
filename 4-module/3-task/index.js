58ccf296d125f3d462c9e176a9d7deed2da1895d
Vitaliya Novikova <vita1980@inbox.ru>
2022-02-10 16:22:43 +0300

    Раздел 5. Задача 3. Строки таблицы

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,29 @@
 function highlight(table) {
-  // ваш код...
+
+  for (let i = 1; i < table.rows.length; i++) {
+
+    let av = table.rows[i].cells[3].getAttribute("data-available");
+    if (av == "true")
+      table.rows[i].classList.add("available");
+    else
+      if (av == "false")
+        table.rows[i].classList.add("unavailable");
+      else
+        if (av === null)
+          table.rows[i].setAttribute("hidden", "hidden");
+
+
+    let ml = table.rows[i].cells[2].textContent;
+    if (ml == 'm')
+      table.rows[i].classList.add("male");
+    else
+      if (ml == 'f')
+        table.rows[i].classList.add("female");
+
+    let age = +table.rows[i].cells[1].textContent;
+    if (age < 18)
+      table.rows[i].style.textDecoration = "line-through";
+
+
+  }
 }
